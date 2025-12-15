f348b33436b3786b7c50ef2a726e9cc9e85e109c
Viren021 <hadawaleviren21@gmail.com>
2025-12-15 14:29:56 +0530

    Create automated test for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,31 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+console.log('Test 1');
+
+if(formatCurrency(2095)==='20.95'){
+  console.log('Passed');
+}else{
+  console.log('Failed');
+}
+
+
+console.log('Test 2');
+if(formatCurrency(0)==='0.00'){
+  console.log('Passed');
+}else{
+  console.log('Failed');
+}
+
+console.log('Test 3');
+if(formatCurrency(2000.5)==='20.01'){
+  console.log('Passed');
+}else{
+  console.log('Failed');
+}
+
+console.log('Test 4');
+if(formatCurrency(2000.4)==='20.00'){
+  console.log('Passed');
+}else{
+  console.log('Failed');
+}
+
